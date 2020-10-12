88360fb63f4eec4d1703c6befe98be73d21cd66b
MalininDD <54958481+MalininDD@users.noreply.github.com>
2020-10-12 19:54:35 +0300

    Add files via upload

---
+++ include/task1.h
@@ -0,0 +1,6 @@
+
+double average(double x,double y);
+bool good(double guess,double x);
+double improve(double guess,double x);
+double iter(double guess,double x);
+double calc(int point,double arg);
